0b53754e1eb2ad825198f28b562da489015782d0
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-05-15 00:16:09 -0500

    [ADD] pantalla principal

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {CalculatorScreen} from './src/screens/CalculatorScreen';
+
+const App = () => {
+  return <CalculatorScreen />;
+};
+export default App;
